1af9d8fcfb2b6a2dab140031b9a1596ed586c087
PranitRout07 <pranitrout72@gmail.com>
2023-12-29 13:52:21 +0530

    modified and added some more test cases

--- main_test.go
+++ main_test.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"log"
 	"testing"
 )
 
-func TestDivide(t *testing.T) {
-	_, err := divide(10.0, 1.0)
-	if err != nil {
-		t.Error("Got an error")
-	}
+var tests = []struct {
+	name     string
+	dividend float64
+	divisor  float64
+	expected float64
+	isErr    bool
+}{
+	{"valid-data", 100.0, 10.0, 10.0, false},
+	{"invalid-data", 100.0, 0.0, 0.0, true},
+	{"valid-data", 90.0, 2.0, 45.0, false},
 }
-func TestBadDivide(t *testing.T) {
-	_, err := divide(10.0, 0)
-	if err == nil {
-		t.Error("0 divide error")
+
+func TestDivision(t *testing.T) {
+	for _, tt := range tests {
+		got, err := divide(tt.dividend, tt.divisor)
+		if tt.isErr {
+			if err == nil {
+				t.Error("Expected an error but did not get one")
+			}
+		} else {
+			if err != nil {
+				t.Error("Not Expected an error but did get one")
+			}
+		}
+
+		if got != tt.expected {
+			t.Errorf("expected %f but got %f", tt.expected, got)
+		}
+		log.Println(tt.name)
+
 	}
 }
