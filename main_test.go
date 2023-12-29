96d0c089899955db7c17316ae41534009ee14eaa
PranitRout07 <pranitrout72@gmail.com>
2023-12-29 14:01:00 +0530

    added both valid and invalid test cases
    added more test cases
    
    more valid test cases are added

--- main_test.go
+++ main_test.go
@@ -15,6 +15,16 @@ var tests = []struct {
 	{"valid-data", 100.0, 10.0, 10.0, false},
 	{"invalid-data", 100.0, 0.0, 0.0, true},
 	{"valid-data", 90.0, 2.0, 45.0, false},
+	{"valid-data",1000.0,20.0,50.0,false},
+	{"invalid-data", 89.0, 0.0, 0.0, true},
+	{"valid-data", 500.0, 2.0, 250.0, false},
+	{"valid-data", 200.0, 4.0, 50.0, false},
+	{"valid-data", 600.0, 2.0, 300.0, false},
+	{"valid-data", 550.0, 11.0, 50.0, false},
+	{"valid-data", 250.0, 2.0, 125.0, false},
+	{"valid-data", 800.0, 2.0, 400.0, false},
+	{"valid-data", 12.0, 2.0, 6.0, false},
+
 }
 
 func TestDivision(t *testing.T) {
